2f5a3c6cb0a3e4fdaed376dfe370f431bdb7c886
Oliwer Lindell <oliwer.lindell@hotmail.com>
2023-06-22 21:08:04 +0200

    Maven publication setup.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,14 @@
 plugins {
-  id("java")
+  java
+  `maven-publish`
 }
 
-group = "com.github.oliwersdk"
-version = "0.0.1"
+val pkg = "com.github.oliwersdk"
+val ver = "0.0.1"
+val exports = arrayOf("java.base/java.util.concurrent=ALL-UNNAMED")
 
-val exports = arrayOf(
-  "java.base/java.util.concurrent=ALL-UNNAMED"
-)
+group = pkg
+version = ver
 
 repositories {
   mavenCentral()
@@ -26,6 +27,17 @@ tasks.withType<JavaCompile> {
 tasks.test {
   useJUnitPlatform()
   jvmArgs(*exports("opens"))
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("maven") {
+      groupId = pkg
+      artifactId = "sew"
+      version = ver
+      from(components["java"])
+    }
+  }
 }
 
 fun exports(type: String = "exports"): Array<String> {
